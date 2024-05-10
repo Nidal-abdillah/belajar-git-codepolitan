5f5e58eeca09e298e6ae8f294be5f84769184ee8
Nidal abdillah <Nidalallabdillah@gmail.com>
2024-05-10 22:07:01 +0700

    final revisi ke 5

--- fungsi.js
+++ fungsi.js
@@ -1,7 +1,11 @@
-console.log("ini fungsi js");
-
-function sum(a, b) {
+let sum = (a, b) => {
   return a + b;
-}
+};
 
 console.log(sum(1, 2));
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+console.log(subtract(1, 2));
--- fungsi2.js
+++
@@ -1,6 +0,0 @@
-const deduct = (a, b) => {
-  return a - b;
-};
-
-console.log(deduct(1, 2));
-console.log(deduct(3, 2));
